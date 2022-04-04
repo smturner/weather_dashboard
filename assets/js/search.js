48d7df3bbc6e266556d37e2c7c83949329ad5e40
Sarah Turner <turnersarah228@gmail.com>
2022-04-04 10:25:41 -0500

    added to local storage and pulled to the screen

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,27 +2,82 @@ var searchBtn =$(".searchBtn");
 var searchCity =$("#search-city");
 var searchInput = $(".search-input");
 var nameEL =$(".cityName");
+var iconEl=$(".icon")
 var tempEl =$(".cityTemp");
 var windEL= $(".cityWind");
 var humidityEl=$(".cityHumidity");
 var indexEl=$(".cityIndex");
+
+var searchHistory=$(".searchHistory")
+var pastCities=[];
+
+// console.log(cityName)
+
 // var indexNumber=$(".indexNumber");
 
 
-searchBtn.on("click", function () {
+searchBtn.on("click", function (searchBtn) {
+    if (searchCity !== ""){
+        var history=JSON.parse(localStorage.getItem("city"))|| [];
+        console.log(history)
+        var cityName = searchCity.val();
+        console.log(cityName)
+    }
+    pastCities.push(cityName)
+    localStorage.setItem("city", JSON.stringify(pastCities))
+
+    // for (var i=0; i<history.length; i++){
+        searchHistory.text(history)
+    }
+    // localStorage.setItem("city", JSON.stringify(pastCities))
+
+    // searchHistory.text(history)
+        // var historySearch=history[i];
+      
+        // console.log(savedHistory)
+        // for (let index = 0; index < highScoresArr.length; index++) {
+        //     var highScore = highScoresArr[index];
+        //     var listItem= document.createElement("li");
+        //     listItem.textContent= `${highScore.initials} - ${highScore.score}`; 
+        //     highScoreList.append(listItem)
+        // function renderHighScore(){
+        //     var startScreenEl = document.getElementById("start-screen");
+        //       startScreenEl.setAttribute("class", "hide") 
+        //     var endScreenEl = document.getElementById("finished-page");
+        //       endScreenEl.setAttribute("class", "hide");
+        //     var highScoreList= document.createElement("ul");
+        //     var highScoreEl =document.getElementById("highScores");
+        //       highScoreEl.setAttribute("class", "show");
+        //     var highScoresArr=JSON.parse(localStorage.getItem("highscores")) || [];
+        //     console.log(highScoresArr)
+        //     for (let index = 0; index < highScoresArr.length; index++) {
+        //       var highScore = highScoresArr[index];
+        //       var listItem= document.createElement("li");
+        //       listItem.textContent= `${highScore.initials} - ${highScore.score}`; 
+        //       highScoreList.append(listItem)
+        //     }
+        
+        //     highScoreEl.append(highScoreList)
+        //    }
+        
+
     getApi()
     //   event.preventDefult();
         // // // //where my button is going
+        // var userSearchCity = searchInput.val();
+
         // document.location.href = "./new.html?search=" + userSearchCity;
-});
+
 // var urlData = document.location.search;
 
 // var searchCityName = urlData.split("=")[1];
 // console.log(searchCityName)
-
+})
+;
 
 function getApi(){
     var userSearchCity = searchCity.val();
+    console.log(userSearchCity)
     var infoUrl= "https://api.openweathermap.org/data/2.5/weather?q=" +userSearchCity+ "&units=imperial&appid=617a9189a649772e2330c08de56b1117"
 fetch(infoUrl)
     .then(function(response){
@@ -30,7 +85,10 @@ fetch(infoUrl)
     })
     .then(function(data){
         console.log(data)
+        // var weatherIcon=(data.weather[0].icon)
+        // console.log(weatherIcon)
         nameEL.text(data.name + "   "+  moment().format("L")) ;
+        // iconEl=`https://openweathermap.org/img/wn/" + weatherIcon+ "&@2x.png`
         tempEl.text("Temperature: " + data.main.temp + "°F");
         windEL.text("Wind: " +data.wind.speed+ " MPH");
         humidityEl.text("Humidity: " +data.main.humidity+ "%")
@@ -72,7 +130,7 @@ fetch(infoUrl)
             })
             .then(function (data){
                 console.log(data.list[""])
-                for(i=5, i<data.list.length, i+8)
+                // for(i=5, i<data.list.length, i+8)
                 
             })
 
@@ -81,3 +139,4 @@ fetch(infoUrl)
     })
 }
 
+ 
